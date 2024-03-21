2f39c53eebc1e4a5209af7ded0f4c2c8f4cf748a
@paglebov_1 <paglebov_1@edu.hse.ru>
2024-03-21 17:38:24 +0300

    Adding Interaction

---
+++ Ozon.AnalyticalSales.Api/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ozon.AnalyticalSales.Domain;
+using Ozon.AnalyticalSales.Domain.Interfaces;
+using Ozon.AnalyticalSales.Service;
+
+var path = Path.Combine(AppContext.BaseDirectory, @"..\..\..\appsettings.json").SettingSeparatorOs();
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile(path, false, true)
+    .Build();
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.Configure<ApplicationOptions>(configuration.GetSection(nameof(ApplicationOptions)));
+builder.Services.AddTransient<IProductService, ProductService>();
+
+var service = builder.Services.BuildServiceProvider();
+
+Console.WriteLine("To stop the calculation process, press Ctrl + c.");
+
+await service.GetService<IProductService>()!.Run();
+
+Console.WriteLine("The calculation process is finished!");
